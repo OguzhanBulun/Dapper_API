b61165e0a2565cfb3572f254976779b4a65e2cf5
Oğuzhan Bulun <oguzhanbulun24@outlook.com>
2024-01-22 23:12:35 +0300

    Add project files.

---
+++ Dapper_UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dapper_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
