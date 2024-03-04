c60dc56aa18e2d181b369b252d8fab9f3a8147a5
Oğuzhan Bulun <oguzhanbulun24@outlook.com>
2024-03-04 22:47:57 +0300

    SecondUpdate 04.03

---
+++ Dapper_UI/ViewComponents/HomePage/_DefaultFooterComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dapper_UI.ViewComponents.HomePage
+
+{
+    public class _DefaultFooterComponentPartial : ViewComponent
+    {   
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
---
+++ Dapper_UI/ViewComponents/HomePage/_DefaultScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultScriptComponentPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
